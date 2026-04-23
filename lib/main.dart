1da2942819ecbbe7cf0e5007adf7fb4473e4d9c3
nabeel0209 <muhammadnabeel0209@gmail.com>
2026-04-23 10:35:17 +0500

    Added Advanced UI Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,165 +1,23 @@
 import 'package:flutter/material.dart';
-
-//
-// void main() {
-//   runApp(
-//     const MaterialApp(
-//       home: Scaffold(
-//         body: Center(child: MyCustomWidget(title: 'My App')),
-//       ),
-//     ),
-//   );
-// }
-//
-// class MyCustomWidget extends StatefulWidget {
-//   const MyCustomWidget({super.key, required this.title});
-//
-//   final String title;
-//
-//   @override
-//   State<MyCustomWidget> createState() => _MyCustomWidgetState();
-// }
-//
-// class _MyCustomWidgetState extends State<MyCustomWidget> {
-//   String _text = 'Hello World';
-//
-//   void changeText() {
-//     setState(() {
-//       _text = 'ahmed is a bad dude';
-//     });
-//   }
-//
-//   void changeTextBackToOriginal() {
-//     setState(() {
-//       _text = 'Hello World';
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         Text(widget.title), // Access widget configuration via 'widget'
-//         Text('New Text: $_text'),
-//         ElevatedButton(
-//           onPressed: changeText,
-//           child: const Text(
-//             'Change Text',
-//             style: TextStyle(
-//               fontSize: 20,
-//               fontFamily: 'Poppins',
-//               color: Colors.deepPurpleAccent,
-//             ),
-//           ),
-//         ),
-//         ElevatedButton(
-//           onPressed: changeTextBackToOriginal,
-//           child: Text(
-//             'Revert Changes',
-//             style: TextStyle(
-//               fontSize: 20,
-//               fontFamily: 'Poppins',
-//               color: Color.fromARGB(255, 74, 20, 140),
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
+import 'package:device_preview/device_preview.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Center(child: ChangingText(title: 'MyApp')),
-      ),
-    ),
-  );
-}
-
-class ChangingText extends StatefulWidget {
-  const ChangingText({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<ChangingText> createState() => ChangingTextState();
-}
-
-class ChangingTextState extends State<ChangingText> {
-  String _text = 'Hello World';
-
-  void changeText() {
-    setState(() {
-      _text = 'ahmed is a bad dude';
-    });
-  }
-
-  void changeTextBackToOriginal() {
-    setState(() {
-      _text = 'Hello World';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Text(widget.title), // Access widget configuration via 'widget'
-        // Container(margin: EdgeInsets.all(50), child: Text('New Text: $_text')),
-        // Container(
-        //   margin: EdgeInsets.only(),
-        //   child: ElevatedButton(
-        //     onPressed: changeText,
-        //     child: const Text(
-        //       'Change Text',
-        //       style: TextStyle(
-        //         fontSize: 20,
-        //         fontFamily: 'Poppins',
-        //         color: Colors.deepPurpleAccent,
-        //       ),
-        //     ),
-        //   ),
-        // ),
-        // ElevatedButton(
-        //   onPressed: changeTextBackToOriginal,
-        //   child: Text(
-        //     'Revert Changes',
-        //     style: TextStyle(
-        //       fontSize: 20,
-        //       fontFamily: 'Poppins',
-        //       color: Color.fromARGB(255, 74, 20, 140),
-        //     ),
-        //   ),
-        // ),
-        Container(
-          width: 800,
-          height: 400,
-          decoration: BoxDecoration(
-            color: Colors.pink,
-            border: Border.all(color: Colors.black, width: 1),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            children: [
-              Text('Hello World'),
-              Container(
-                child: Row(
-                  children: [
-                    Image.asset('youtube.png', width: 150, height: 150),
-                    Image.network(
-                      'https://as2.ftcdn.net/v2/jpg/17/65/88/21/1000_F_1765882144_LIGjRITDUSaBTelN8vQ2WCsaL1qP3eCv.webp',
-                      width: 400,
-                      height: 400,
-                    ),
-                  ],
+      DevicePreview(
+          enabled: true,
+          builder: (context) =>
+              MaterialApp(
+                useInheritedMediaQuery: true,
+                debugShowCheckedModeBanner: false,
+                home: Scaffold(
+                  appBar: AppBar(title: Text('My Application')),
+                  body: ListView(
+                    children: [
+                      ListTile(leading: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/3840px-Instagram_logo_2016.svg.png', width: 50, height: 50,), title: Text('Muhammad Nabeel'), trailing: Icon(Icons.person_add_alt_1, color: Colors.black, size: 30,),),
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
+      )
+  );
 }
