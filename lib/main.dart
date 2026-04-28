1dc1ca14aa655d0ed1d49b9d6d69d24e260a243c
nabeel0209 <muhammadnabeel0209@gmail.com>
2026-04-28 09:59:17 +0500

    Improved layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:device_preview/device_preview.dart';
-//
-// void main() {
-//   runApp(
-//     DevicePreview(
-//       enabled: true,
-//       builder: (context) => MaterialApp(
-//         useInheritedMediaQuery: true,
-//         debugShowCheckedModeBanner: false,
-//         home: Scaffold(
-//           appBar: AppBar(title: Text('My Application')),
-//           body: ListView(
-//             children: [
-//               ListTile(
-//                 leading: Image.asset('youtube.png', width: 50,height: 50),
-//                 title: Text(
-//                   'Another iteration of mind.png',
-//                   style: TextStyle(
-//                     fontWeight: FontWeight.bold,
-//                     fontSize: 16,
-//                     color: Color(0xFF2D3A4A),
-//                   ),
-//                 ),
-//                 subtitle: Text(
-//                   '1.3 MB • Received',
-//                   style: TextStyle(color: Colors.grey, fontSize: 13),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-// }
-//
-//
-//
-//
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -51,10 +10,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // Controllers
-
   TextEditingController userNameController = TextEditingController();
-  TextEditingController fatherNameController = TextEditingController();
   TextEditingController ageController = TextEditingController();
 
   List<Map<String, dynamic>> users = [];
@@ -66,47 +22,63 @@ class _MyAppState extends State<MyApp> {
         title: Text('User Form', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.black,
       ),
-      body: Container(
-        width: 500,
-        padding: EdgeInsets.all(20),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Text('User Name'),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: TextField(
-                      controller: userNameController,
-                      decoration: InputDecoration(
-                        hintText: 'Enter your username',
-                        border: OutlineInputBorder(),
+
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('User Name'),
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: TextField(
+                          controller: userNameController,
+                          decoration: InputDecoration(
+                            hintText: 'Enter your username',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Text('User Age'),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: TextField(
-                      controller: userNameController,
-                      decoration: InputDecoration(
-                        hintText: 'Enter your age',
-                        border: OutlineInputBorder(),
+                ),
+
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('User Age'),
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: TextField(
+                          controller: ageController,
+                          decoration: InputDecoration(
+                            hintText: 'Enter your age',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: ElevatedButton(
+                    onPressed: () => print('Hello World'),
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    child: Text('Add', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
